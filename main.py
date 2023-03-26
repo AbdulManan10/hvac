c615048bc7c494373b8fe7cb2fba75abe5252582
AbdulManan10 <74857982+AbdulManan10@users.noreply.github.com>
2023-03-26 16:35:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from hvac import Client
 
 client = Client(
-    url='https://vault-cluster-public-vault-388d931b.043735e3.z1.hashicorp.cloud:8200',
+    url='Your-Cluster-URL',
     namespace='admin',
     verify=False
 )
 
 
 client.auth.approle.login(
-    role_id='dc1922ba-d452-5672-9c12-eb7b5442c099',
-    secret_id='1f9960f4-cebc-babd-b50a-de6a88a41d1f'
+    role_id='Your-Role-Id',
+    secret_id='Your-Secret-Id'
 )
 
-secret = client.read('secret/data/ingestion/dev/auth/decentraland')
+secret = client.read('Your-Secret-Path')
 print(secret)
 
 
